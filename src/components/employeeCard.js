156caf7bdf7605e0da7c418220f80ca0461a05e5
ggstoltz <gunnar.b.stoltz@gmail.com>
2021-02-19 14:34:21 -0600

    Building
    Cleaned up unused files, imported bootstrap, started working on components

---
+++ src/components/employeeCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function employeeCard(props) {
+    return (
+        // <div className='row'>
+        <tr>
+           {/* <div className= "col-2"> */}
+      {/* <td > */}
+      <th scope="row"><img alt={props.firstName}  src={props.picture} /></th>  
+      {/* </td>  */}
+      {/* </div>
+      <div className= "col-2"> */}
+      <td >{props.firstName}</td>
+      {/* </div>
+      <div className= "col-2"> */}
+      <td >{props.lastName}</td>
+      {/* </div>
+      <div className= "col-3"> */}
+      <td >{props.email}</td>
+      {/* </div>
+      <div className= "col-3"> */}
+      <td >{props.phone}</td>
+      {/* </div> */} 
+        </tr>
+        // </div>
+    );
+
+
+}
+
+export default employeeCard;
